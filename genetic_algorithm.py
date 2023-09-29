98c74099c9d1b052fddaa7a5d69a002919e80eba
Collin-Fox <131672263+Collin-Fox@users.noreply.github.com>
2023-09-29 13:57:45 -0500

    Still need to do the mutations and look at the probability of selection because all the probability is so close it seems to be choosing almost randomly and not the best with proportion after first reproduce cycle

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -18,7 +18,7 @@ def fitness(n_queen):
     return num_conflicts
 
 
-def generate_initial_population(n: int, population_size: int):
+def generate_initial_population(n: int):
     """
 
     :param population_size:
@@ -31,6 +31,7 @@ def generate_initial_population(n: int, population_size: int):
     """
     # Create the initial string for N queens
     initial = ""
+    population_size = int((n * n) * np.ceil(np.log2(n)))
     for i in range(1, n + 1):
         initial += str(i)
 
@@ -42,12 +43,146 @@ def generate_initial_population(n: int, population_size: int):
     # Selecting N random elements from the set for initial population
     population_list = random.sample(list(perms), population_size)
 
-    print(population_list)
-
     return population_list
 
 
-x = generate_initial_population(9, 15)
-print(fitness(x[0]))
-print(fitness(x[1]))
-print(fitness(x[2]))
+def sort_by_fitness(population):
+    """
+
+    :param population:
+    :return:
+    """
+    population.sort(key=fitness)
+    return population
+
+
+def get_total_fitness(population):
+    return sum([fitness(state) for state in population])
+
+
+def choose_parents(population):
+    total = get_total_fitness(population)
+    breeding_size = int(len(population) / 10)
+    breeding_set = []
+    while len(breeding_set) < breeding_size:
+        for state in population:
+            fit = fitness(state)
+            probability = (len(state) * (len(state) - 1) - fit)
+            random_gen = random.randint(0, total)
+            if len(set(breeding_set)) < breeding_size and random_gen <= probability:
+                breeding_set.append(state)
+    return set(breeding_set)
+
+
+def get_breeding_pairs(breeding_set):
+    total_fitness = get_total_fitness(breeding_set)
+    breeding_pairs_list = []
+    while len(breeding_pairs_list) < 6:
+        for state in breeding_set:
+            probability = (len(state) * (len(state) - 1)) - fitness(state)
+            random_gen = random.randint(1, total_fitness)
+            if len(set(breeding_pairs_list)) < 6 and random_gen <= probability:
+                breeding_pairs_list.append(state)
+
+    breeding_pairs = [(breeding_pairs_list[0], breeding_pairs_list[1]),
+                      (breeding_pairs_list[2], breeding_pairs_list[3]),
+                      (breeding_pairs_list[4], breeding_pairs_list[5])]
+    return breeding_pairs
+
+
+def crossover(breeding_pairs):
+    i = -1
+    j = -1
+
+    # Breeding pairs
+    a, b = breeding_pairs
+
+    # Randomly select two indices
+    while i == j:
+        i = random.randint(0, len(a))
+        j = random.randint(0, len(a))
+
+    # Make sure i < j
+    min_index = min(i, j)
+    max_index = max(i, j)
+
+    # Get the substrings
+    p0_retained = a[min_index:max_index]
+    p0_left = a[:min_index]
+    p0_right = a[max_index:]
+    p0_replaced = p0_left + p0_right
+
+    # Replace the substring in the other parent
+    holder = b
+    p1_retained = ""
+    p1_replaced = ""
+
+    # Replace the substring in the other parent
+    for letter in holder:
+        if letter in p0_retained:
+            p1_retained += letter
+        else:
+            holder = holder.replace(letter, "?")
+            p1_replaced += letter
+
+    # Replace the positions needed to replace w ?'s
+    replaceHolder = ""
+    for i in range(0, min_index):
+        replaceHolder += "?"
+
+    p0_retained = replaceHolder + p0_retained
+    for i in range(max_index, len(a)):
+        p0_retained += "?"
+
+    # Replace the positions in p1 needed to replace w p0s extras
+    for letter in holder:
+        if letter == "?":
+            holder = holder.replace("?", p0_replaced[0], 1)
+            p0_replaced = p0_replaced[1:]
+
+    # Replace the positions in p0 needed to replace w p1s extras
+    for letter in p0_retained:
+        if letter == "?":
+            p0_retained = p0_retained.replace("?", p1_replaced[0], 1)
+            p1_replaced = p1_replaced[1:]
+
+    child1 = p0_retained
+    child2 = holder
+
+    return child1, child2
+
+
+def run_genetic_algorithm():
+    x = generate_initial_population(9)
+    x = sort_by_fitness(x)
+    breeding_set = sort_by_fitness(list(choose_parents(x)))
+    counter = 0
+    print("Initital Population")
+    for state in breeding_set:
+        print(state, "Fitness", fitness(state))
+
+    while counter < 50:
+        breeding_pairs = get_breeding_pairs(breeding_set)
+        print(breeding_pairs)
+        breeding_children = []
+        while len(breeding_children) < 50:
+            for pair in breeding_pairs:
+                child1, child2 = crossover(pair)
+                if child1 not in breeding_children and len(breeding_children) < 50:
+                    breeding_children.append(child1)
+                if child2 not in breeding_children and len(breeding_children) < 50:
+                    breeding_children.append(child2)
+
+        breeding_children = sort_by_fitness(breeding_children)
+
+        for state in breeding_children:
+            print(state, "Fitness", fitness(state))
+            if fitness(state) == 0:
+                print("N Queens Solution Found", state)
+                return state
+
+        counter += 1
+        breeding_pairs = breeding_children
+
+
+run_genetic_algorithm()
--- main.py
+++ main.py
@@ -1,114 +1,59 @@
 import random
 
-def generate_random_permutation(n):
-  """Generates a random permutation of a sequence of numbers from 1 to n.
+def is_valid_state(state):
+  """Checks if a given state is valid.
 
   Args:
-    n: The number of elements in the permutation.
+    state: A string representing the state of the board.
 
   Returns:
-    A list containing a random permutation of the numbers from 1 to n.
+    True if the state is valid, False otherwise.
   """
 
-  permutation = list(range(1, n + 1))
-  random.shuffle(permutation)
-  return permutation
+  for i in range(len(state)):
+    for j in range(i + 1, len(state)):
+      if state[i] == state[j] or abs(i - j) == abs(state[i] - state[j]):
+        return False
+  return True
 
-def fitness(permutation):
-  """Computes the fitness of a permutation.
-
-  Args:
-    permutation: A list containing a permutation of the numbers from 1 to n.
-
-  Returns:
-    The number of conflicts in the permutation.
-  """
-
-  conflicts = 0
-  for i in range(len(permutation)):
-    for j in range(i + 1, len(permutation)):
-      if permutation[i] == permutation[j] or abs(i - j) == abs(permutation[i] - permutation[j]):
-        conflicts += 1
-  return conflicts
-
-def crossover(parent1, parent2):
-  """Performs crossover on two permutations.
-
-  Args:
-    parent1: A list containing a permutation of the numbers from 1 to n.
-    parent2: A list containing a permutation of the numbers from 1 to n.
-
-  Returns:
-    Two child permutations, each of which is a combination of the two parents.
-  """
-
-  crossover_point = random.randint(1, len(parent1) - 1)
-  child1 = parent1[:crossover_point] + parent2[crossover_point:]
-  child2 = parent2[:crossover_point] + parent1[crossover_point:]
-
-  return child1, child2
-
-def mutation(permutation):
-  """Performs mutation on a permutation.
-
-  Args:
-    permutation: A list containing a permutation of the numbers from 1 to n.
-
-  Returns:
-    A mutated permutation.
-  """
-
-  mutation_point1 = random.randint(0, len(permutation) - 1)
-  mutation_point2 = random.randint(0, len(permutation) - 1)
-  while mutation_point1 == mutation_point2:
-    mutation_point2 = random.randint(0, len(permutation) - 1)
-
-  permutation[mutation_point1], permutation[mutation_point2] = permutation[mutation_point2], permutation[mutation_point1]
-
-  return permutation
-
-def genetic_algorithm(n, breeding_set_size, num_generations, num_breeding_pairs, mutation_probability, num_breeding_attempts):
-  """Solves the N-Queens problem using a genetic algorithm.
+def generate_state(n):
+  """Generates a random state for the N-queens problem.
 
   Args:
     n: The number of queens.
-    breeding_set_size: The size of the breeding set.
-    num_generations: The number of generations of reproduction.
-    num_breeding_pairs: The number of breeding pairs.
-    mutation_probability: The probability of mutation.
-    num_breeding_attempts: The number of breeding attempts.
 
   Returns:
-    A list containing a solution to the N-Queens problem, or None if no solution was found.
+    A string representing the state of the board.
   """
 
-  # Generate a random population sample.
-  population_sample = [generate_random_permutation(n) for i in range(breeding_set_size)]
+  state = ""
+  for i in range(n):
+    state += str(random.randint(0, n - 1))
+  return state
 
-  # Compute the fitness of each permutation in the population sample.
-  for i in range(len(population_sample)):
-    population_sample[i] = (population_sample[i], fitness(population_sample[i]))
+def generate_states(n, x):
+  """Generates x random states for the N-queens problem.
 
-  # Sort the population sample by fitness.
-  population_sample.sort(key=lambda x: x[1])
+  Args:
+    n: The number of queens.
+    x: The number of states to generate.
 
-  # Initialize the breeding set.
-  breeding_set = population_sample[:breeding_set_size]
+  Returns:
+    A list of strings representing the states of the board.
+  """
 
-  # For each generation, perform reproduction and selection on the breeding set.
-  for generation in range(num_generations):
+  states = []
+  for i in range(x):
+    states.append(generate_state(n))
+  return states
 
-    # Select a random subset of the breeding set to breed.
-    breeding_pairs = random.sample(breeding_set, num_breeding_pairs)
 
-    # Create offspring from the breeding pairs.
-    offspring = []
-    for parent1, parent2 in breeding_pairs:
-      child1, child2 = crossover(parent1, parent2)
 
-      # Mutate the offspring with a probability of mutation_probability.
-      if random.random() < mutation_probability:
-        child1 = mutation(child1)
-      if random.random() < mutation_probability:
-        child12 = mutation(child2)
+if __name__ == "__main__":
+  n = int(input("Enter the number of queens: "))
+  x = int(input("Enter the number of states to generate: "))
 
+  states = generate_states(n, x)
+
+  for state in states:
+    print(state)
